651d4ea0222bf4c93a6d1fa5a90db9d0166445e3
ian <ianbean911@gmail.com>
2023-05-17 23:36:40 -0400

    Split keystrokes into left and right hand, add holding stack rather than single block, change scoring. Speedups still need testing.

--- main.c
+++ main.c
@@ -14,16 +14,45 @@
 
 static const Color colors[] = {RED, GREEN, BLUE, VIOLET};
 
+//typedef struct {
+//    Vector2 position;
+//    Vector2 velocity;
+//    float opacity;
+//    uint64_t points;
+//} PointParticle;
+
+typedef struct {
+    Vector2 position;
+    Vector2 velocity;
+    Color color;
+} BlockParticle;
+
 typedef struct {
     int8_t next;
     int8_t stack[STACK_SIZE];
-    uint8_t stackHeight;
-    float positions[STACK_SIZE];
+    uint8_t size;
+    /* floats in terms of blockSizes */
+    float offset;  /* how far the next block has grown */
+    float positions[STACK_SIZE];  /* y added to last block y */
+    float velocities[STACK_SIZE];
 } Stack;
+
+static Stack stacks[STACK_COUNT] = {0};
+static Stack holding = {0};
+
+static Sound fxPlace;
+static Sound fxPickup;
+static Sound fxMisplace;
+static Sound fxSpeedup;
+static Sound fxGameover;
+static Sound fxPop;
+
+void pickup(uint32_t stackIndex);
+void place(uint32_t stackIndex);
+unsigned int pop();
 
 int main(void) {
     /* Stack initialization */
-    Stack stacks[STACK_COUNT] = {0};
     for (int i = 0; i < STACK_COUNT; i++) {
         stacks[i] = (Stack) {0};
         for (int j = 0; j < STACK_SIZE; j++) {
@@ -31,19 +60,26 @@ int main(void) {
         }
         stacks[i].next = GetRandomValue(0, 3);
     }
+    for (int i = 0; i < STACK_SIZE; i++) {
+        holding.stack[i] = -1;
+    }
+
+//    PointParticle pointParticles[64] = {0};
+    unsigned int pointParticleIndex = 0;
 
     /* gamestate variables */
-    float growOffset = 0.0f;
     float growSpeed = BASE_SPEED;
-    char holding = -1;
+    float speedBoost = 10.0f;
     int score = 0;
-    int scoreGoal = BASE_POINTS * 10;
+    int scoreGoal = BASE_POINTS * 100;
     int level = 1;
     int gameOver = 0;
 
     /* drawing variables */
-    int screenWidth = DEFAULT_SCREEN_WIDTH;
-    int screenHeight = DEFAULT_SCREEN_HEIGHT;
+    uint32_t screenWidth = DEFAULT_SCREEN_WIDTH;
+    uint32_t screenHeight = DEFAULT_SCREEN_HEIGHT;
+    uint32_t screenX = 0;
+    uint32_t screenY = 0;
     char textBuffer[128] = {0};
     int stackPadding = 10;
     int blockSize = screenHeight / 20;
@@ -51,16 +87,15 @@ int main(void) {
     int fieldHeight = blockSize * STACK_SIZE;
     Rectangle field = (Rectangle){(screenWidth - fieldWidth) / 2,
                                   screenHeight - (fieldHeight + (2 * blockSize)),
-                                  fieldWidth,
-                                  fieldHeight};
+                                  fieldWidth, fieldHeight};
     /* init audio */
     InitAudioDevice();
-    Sound fxPlace = LoadSound("resources/place.wav");
-    Sound fxPickup = LoadSound("resources/pickup.wav");
-    Sound fxMisplace = LoadSound("resources/misplace.wav");
-    Sound fxSpeedup = LoadSound("resources/speedup.wav");
-    Sound fxGameover = LoadSound("resources/gameover.wav");
-    Sound fxPop = LoadSound("resources/pop.wav");
+    fxPlace = LoadSound("resources/place.wav");
+    fxPickup = LoadSound("resources/pickup.wav");
+    fxMisplace = LoadSound("resources/misplace.wav");
+    fxSpeedup = LoadSound("resources/speedup.wav");
+    fxGameover = LoadSound("resources/gameover.wav");
+    fxPop = LoadSound("resources/pop.wav");
 
     /* init window */
     float frameTime;
@@ -72,99 +107,53 @@ int main(void) {
         frameTime = GetFrameTime();
 
         if (!gameOver) {
-            /* update growOffset and push to bottom of Stack when ready */
-            if (growOffset >= 1.0f) {
-                for (int i = 0; i < STACK_COUNT; i++) {
-                    stacks[i].stackHeight++;
+            /* update offset and push to bottom of Stack when ready */
+            for (int i = 0; i < STACK_COUNT; i++) {
+                Stack *s = &(stacks[i]);
+                s->offset += growSpeed * frameTime * ((s->size == 0) * speedBoost + 1);
+                if (s->offset >= 1.0f) {
+                    s->offset -= 1.0f;
+                    s->size++;
                     for (int j = STACK_SIZE - 1; j > 0; j--) {
-                        stacks[i].stack[j] = stacks[i].stack[j - 1];
+                        s->stack[j] = s->stack[j - 1];
                     }
-                    stacks[i].stack[0] = stacks[i].next;
-                    stacks[i].next = (char) GetRandomValue(0, 3);
+                    s->stack[0] = s->next;
+                    s->next = (char) GetRandomValue(0, 3);
                 }
-                growOffset = 0.0f;
             }
-            growOffset += growSpeed * frameTime;
+
 
             /* handle input */
-            int stackPick = -1;
-            if (IsKeyPressed(KEY_J)) {
-                stackPick = 0;
-            }
-            else if (IsKeyPressed(KEY_K)) {
-                stackPick = 1;
-            }
-            else if (IsKeyPressed(KEY_L)) {
-                stackPick = 2;
-            }
-            if (stackPick != -1) {
-                Stack *s = &stacks[stackPick];
-                if (holding == -1) {  /* if pressed a Stack button and not holding anything */
-                    if (s->stackHeight > 0) {  /* if there's at least one block above ground */
-                        holding = s->stack[s->stackHeight - 1];
-                        s->stack[s->stackHeight - 1] = -1;
-                        s->stackHeight--;
-                        PlaySound(fxPickup);
-                    }
-                    else {
-                        PlaySound(fxMisplace);
-                    }
-                }
-                else {
-                    s->stack[s->stackHeight] = holding;
-                    s->stackHeight++;
-                    holding = -1;
-                    PlaySound(fxPlace);
-                }
-            }
+            if (IsKeyPressed(KEY_J)) { place(0); }
+            else if (IsKeyPressed(KEY_K)) { place(1); }
+            else if (IsKeyPressed(KEY_L)) { place(2); }
 
-            /* check for 3 in a rows */
+            if (IsKeyPressed(KEY_S)) { pickup(0); }
+            else if (IsKeyPressed(KEY_D)) { pickup(1); }
+            else if (IsKeyPressed(KEY_F)) { pickup(2); }
+
+            /* pop 3+ in a row */
             if (IsKeyPressed(KEY_SPACE)) {
-                int popped = 0;
-                for (int i = 0; i < STACK_COUNT; i++) {
-                    int run = 1;
-                    char lastColor = -1;
-                    for (int j = 0; j < STACK_SIZE; j++) {
-                        if (stacks[i].stack[j] != -1 && stacks[i].stack[j] == lastColor) {
-                            run++;
-                        }
-                        else {
-                            if (run >= 3) {
-                                popped += run;
-                                for (int k = j - run; k < STACK_SIZE; k++) {
-                                    if (k + run >= STACK_SIZE) {
-                                        stacks[i].stack[k] = -1;
-                                    }
-                                    else {
-                                        stacks[i].stack[k] = stacks[i].stack[k + run];
-                                    }
-                                }
-                                stacks[i].stackHeight -= run;
-                                j = 1;
-                                if (score >= scoreGoal) {
-                                    growSpeed *= 1.2f;
-                                    scoreGoal += BASE_POINTS * 10 * powf(1.2, level - 1);
-                                    level++;
-                                    PlaySound(fxSpeedup);
-                                }
-                            }
-                            run = 1;
-                            lastColor = stacks[i].stack[j];
-                        }
-                    }
-                }
-                if (popped > 0) {
-                    score += BASE_POINTS * powf(1.2, popped - 1);
+                uint32_t points = pop();
+                score += points;
+                if (points > 0) {
                     PlaySound(fxPop);
-                }
-                else {
+                } else {
                     PlaySound(fxMisplace);
+                }
+                if (score >= scoreGoal) {
+                    PlaySound(fxSpeedup);
+                }
+                while (score >= scoreGoal) {
+                    growSpeed *= 1.1f;
+                    scoreGoal += BASE_POINTS * 100 * powf(1.2, level);
+                    level++;
                 }
             }
 
             /* check for gameover */
             for (int i = 0; i < STACK_COUNT; i++) {
-                if (stacks[i].stackHeight >= STACK_SIZE) {
+                if (stacks[i].size >= STACK_SIZE) {
                     PlaySound(fxGameover);
                     gameOver = 1;
                     break;
@@ -172,21 +161,48 @@ int main(void) {
             }
         }
 
+        /* do physics */
+//        for (int i = 0; i < STACK_COUNT; i++) {
+//            for (int j = 0; j < STACK_SIZE; j++) {
+//                stacks[i].velocities[j] -= 1.0f * frameTime;
+//                stacks[i].positions[j] += stacks[i].velocities[j] * frameTime;
+//                float miny = stacks[i].offset + (j * blockSize);
+//                if (stacks[i].positions[j] < miny) {
+//                    stacks[i].positions[j] = miny;
+//                    stacks[i].velocities[j] = 0.0f;
+//                }
+//            }
+//        }
+//        for (int i = 0; i < 64; i++) {
+//            pointParticles[i].velocity.y += 1.0f * frameTime;
+//            pointParticles[i].position.y += pointParticles[i].velocity.y * frameTime;
+//            pointParticles[i].position.x += pointParticles[i].velocity.x * frameTime;
+//            pointParticles[i].opacity -= 0.1f * frameTime;
+//        }
+
+
+        /* handle window resizes */
         if (IsWindowResized()) {
             screenWidth = GetScreenWidth();
             screenHeight = GetScreenHeight();
-            if (screenWidth > (screenHeight * (9.0f/16.0f))) {
-                screenWidth = screenHeight * (9.0f/16.0f);
+            uint32_t maxWidth = screenHeight * (9.0f/16.0f);
+            uint32_t maxHeight = screenWidth * (9.0f/16.0f);
+            if (screenWidth > maxWidth) {
+                screenX = (screenWidth - maxWidth) / 2;
+                screenY = 0;
+                screenWidth = maxWidth;
             }
-            if (screenHeight > (screenWidth * (16.0f/9.0f))) {
-                screenHeight = screenWidth * (16.0f/9.0f);
+            if (screenHeight > maxHeight) {
+                screenX = 0;
+                screenY = (screenHeight - maxHeight) / 2;
+                screenHeight = maxHeight;
             }
             blockSize = screenHeight / 20;
             stackPadding = blockSize / 4;
             fieldWidth = (stackPadding * 2 + blockSize) * STACK_COUNT;
             fieldHeight = blockSize * STACK_SIZE;
-            field = (Rectangle){(screenWidth - fieldWidth) / 2,
-                                screenHeight - (fieldHeight + (2 * blockSize)),
+            field = (Rectangle){screenX + ((screenWidth - fieldWidth) / 2),
+                                screenY + (screenHeight - (fieldHeight + (2 * blockSize))),
                                 fieldWidth,
                                 fieldHeight};
         }
@@ -195,24 +211,36 @@ int main(void) {
         {
             ClearBackground(RAYWHITE);
             DrawRectangleRec(field, GRAY);
-            DrawRectangle((screenWidth - blockSize)/2, stackPadding, blockSize, blockSize, colors[holding]);
+            for (int i = 0; i < STACK_SIZE; i++) {
+                /* draw holding stack */
+                if (holding.stack[i] == -1) {
+                    break;
+                }
+                DrawRectangle(field.x + fieldWidth + stackPadding,
+                              field.y + fieldHeight - (i * blockSize),
+                              blockSize, blockSize, colors[holding.stack[i]]);
+            }
             for (int i = 0; i < STACK_COUNT; i++) {
+                /* draw next */
                 Color c = colors[stacks[i].next];
                 c.r /= 2;
                 c.g /= 2;
                 c.b /= 2;
                 DrawRectangle(field.x + stackPadding + ((stackPadding * 2 + blockSize) * i),
-                              field.y + fieldHeight - ((growOffset) * blockSize),
+                              field.y + fieldHeight - ((stacks[i].offset) * blockSize),
                               blockSize, blockSize, c);
+                /* draw stack */
+                float y = stacks[i].offset;
                 for (int j = 0; j < STACK_SIZE; j++) {
+                    y += 1;
                     if (stacks[i].stack[j] == -1) {
-                        //assert(stacks[i].stackHeight == j);
+                        assert(stacks[i].size == j);
                         break;
                     }
                     c = colors[stacks[i].stack[j]];
+                    y += stacks[i].positions[j];
                     DrawRectangle(field.x + stackPadding + ((stackPadding * 2 + blockSize) * i),
-                                  field.y + fieldHeight - ((growOffset + 1 + j) * blockSize),
-                                  blockSize, blockSize, c);
+                                  field.y + fieldHeight - (y * blockSize), blockSize, blockSize, c);
                 }
             }
             int fontSize = blockSize / 2;
@@ -233,8 +261,8 @@ int main(void) {
             DrawText("l", lTextPos, screenHeight - fontSize - stackPadding, fontSize, DARKGRAY);
             if (gameOver) {
                 int gowidth = MeasureText("game over", fontSize);
-                DrawText("game over", (screenWidth - gowidth)/2 , 10, fontSize, DARKGRAY);
-
+                DrawText("game over",field.x + ((field.width - gowidth) / 2),
+                         field.y + (field.width / 2), fontSize, DARKGRAY);
             }
             DrawRectangle(field.x, field.y + field.height, field.width, blockSize, DARKGRAY);
         }
@@ -250,4 +278,68 @@ int main(void) {
     CloseWindow();
 
     return 0;
+}
+
+void pickup(uint32_t stackIndex) {
+    Stack *s = &stacks[stackIndex];
+    if (holding.size < STACK_SIZE && s->size > 0) {
+        holding.stack[holding.size] = s->stack[s->size - 1];
+        holding.size++;
+        s->stack[s->size - 1] = -1;
+        s->size--;
+        PlaySound(fxPickup);
+    }
+    else {
+        PlaySound(fxMisplace);
+    }
+}
+
+void place(unsigned int stackIndex) {
+    Stack *s = &stacks[stackIndex];
+    if (holding.size > 0) {
+        s->stack[s->size] = holding.stack[holding.size - 1];
+        s->size++;
+        holding.stack[holding.size - 1] = -1;
+        holding.size--;
+        PlaySound(fxPlace);
+    }
+    else {
+        PlaySound(fxMisplace);
+    }
+}
+
+uint32_t pop() {
+    uint32_t popped = 0;  /* number of blocks popped */
+    uint32_t points = BASE_POINTS;  /* points per block */
+    uint32_t score = 0;  /* total score from this pop */
+    for (int i = 0; i < STACK_COUNT; i++) {
+        Stack *s = &stacks[i];
+        int run = 1;
+        char lastColor = -1;
+        for (int j = 0; j < STACK_SIZE; j++) {
+            if (s->stack[j] != -1 && s->stack[j] == lastColor) {
+                run++;
+            }
+            else {
+                if (run >= 3) {
+                    popped += run;
+                    for (int k = j - run; k < STACK_SIZE; k++) {
+                        score += points;
+                        points *= 1.1f;
+                        if (k + run >= STACK_SIZE) {
+                            s->stack[k] = -1;
+                        }
+                        else {
+                            s->stack[k] = s->stack[k + run];
+                        }
+                    }
+                    s->size -= run;
+                    j = 1;
+                }
+                run = 1;
+                lastColor = s->stack[j];
+            }
+        }
+    }
+    return score;
 }
